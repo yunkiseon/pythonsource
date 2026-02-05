00efcf825c7b52e449821efd21b4addecae46b3a
yunkiseon <yks97016033@gmail.com>
2026-02-05 17:54:08 +0900

    문자열 타입과 함수, 리스트

--- basic/basic.py
+++ basic/basic.py
@@ -2,5 +2,14 @@
 print("파이썬 프로그래밍")
 
 # 키보드에서 입력받기
-msg = input("input num : ")
-print(msg)
+# msg = input("input num : ")
+# print(msg)
+# 원래는 홑따옴표로 적어도 Black Formatter 라이브러리 사용하면 쌍따옴표로 정리해줌
+str1 = "Life is too short, You need Python"
+str1 = "Life is too short, You need Python"
+str1 = """Life is too short, You need Python"""
+str1 = """Life is too short,\
+You need Python"""
+
+
+print(str1)
